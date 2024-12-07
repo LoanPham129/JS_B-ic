f1b46612d385e04dfd7be493ec2c1a69b315365b
LoanPham129 <phamphuongloan04@gmail.com>
2024-12-07 09:43:17 +0700

    method string

---
+++ b2/methodString.js
@@ -0,0 +1,6 @@
+// dấu () dùng khi truyền value
+// dấu [] dùng khi truyền key
+// dấu {} dùng khi truyền object
+
+let z = '1$2$3$4$5'
+console.log(z)
---
+++ b2/string.js
@@ -0,0 +1,16 @@
+let a='this is string'
+console.log(a)
+
+//chỉ cộng chuỗi
+console.log(4 + 5 + 'a')
+console.log('a' + 4 + 5)
+
+//khi khai bao có ' or " thì giải quyết bằng ``
+// cách khác \' or \"
+// dấu `` có thể xuống dòng
+let b = `soothing blue`
+console.log(b)
+
+let b1 = `soothing
+ blue`
+console.log(b1)
