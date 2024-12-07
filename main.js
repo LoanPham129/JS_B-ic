d03eef9f982a8c087af2d83f62680a20918cac5a
LoanPham129 <phamphuongloan04@gmail.com>
2024-12-07 09:02:34 +0700

    bai 1

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log('hi em')
+
+let c = 5
+let d = 2.5
+console.log(c + d)
+
+let x=3, y=4, z=5
+console.log(x+y+z)
+console.log(`x=`,x, `y=`,y,`z=`,z)
+
+let name = 21 + 'Loan'
+let name1 = 'Loan' + 6
+console.log(name)
+console.log(name1)
